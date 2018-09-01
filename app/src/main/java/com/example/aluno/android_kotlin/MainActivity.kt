6ca7cf049d8d6df385bb173e490450185e3772aa
Emanuelbsl <emanuel_bsl@hotmail.com>
2018-09-01 12:27:19 -0300

    atualizando projeto

--- app/src/main/java/com/example/aluno/android_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/aluno/android_kotlin/MainActivity.kt
@@ -2,11 +2,45 @@ package com.example.aluno.android_kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("onCreate","onCreate")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("onStart","onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("onResume","onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("onPause","onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("onStop","onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("onRestart","onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("onDestroy","onDestroy")
+    }
+
 }
